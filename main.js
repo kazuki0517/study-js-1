a574bbd95840e1e2e5943fea6446d3d84f78697a
kazuki0517 <kh2328pkh@gmail.com>
2023-03-08 08:19:56 +0900

    study-js-#1 done

---
+++ main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+{
+ const btn = document.getElementById('btn');
+  const result =document.getElementById('result')
+
+
+ btn.addEventListener('click', ()=> {
+  const results = ['大吉','中吉','凶','小吉'];
+  const n = Math.floor(Math.random()*results.length);
+  // console.log(math);
+  result.textContent = results[n];
+  // switch(n) {
+  //  case 2 : result.textContent = "大吉";
+  //  break;
+  //  case 1 : result.textContent = "中吉";
+  //  break;
+  //  case 0 : result.textContent = "凶";
+  //  break;
+  // }
+ });
+}
